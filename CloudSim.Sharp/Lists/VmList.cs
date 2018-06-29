8466689540edef0c4ad95c9d969edf5c618ac88e
vkolesnik <klevalera@gmail.com>
2018-06-29 14:22:24 +0300

    Add extension methods for List<Vm>

---
+++ CloudSim.Sharp/Lists/VmList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSim.Sharp.Lists
+{
+    public static class VmList
+    {
+        public static Vm GetById(this List<Vm> vmList, int id)
+        {
+            return vmList.FirstOrDefault(vm => vm.Id == id);
+        }
+
+        public static Vm GetByIdAndUserId(this List<Vm> vmList, int id, int userId)
+        {
+            return vmList.FirstOrDefault(vm => vm.Id == id && vm.UserId == userId);
+        }
+    }
+}
