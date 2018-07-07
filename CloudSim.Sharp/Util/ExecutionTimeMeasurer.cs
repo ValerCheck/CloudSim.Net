58cf3e72c855b6b34b8f75702e8e1e542e50f052
vkolesnik <klevalera@gmail.com>
2018-07-07 16:02:29 +0300

    Add ExecutionTimeMeasurer.

---
+++ CloudSim.Sharp/Util/ExecutionTimeMeasurer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CloudSim.Sharp.Util
+{
+    public class ExecutionTimeMeasurer
+    {
+        private static readonly Dictionary<string, long> _executionTimes = new Dictionary<string, long>();
+
+        private static long GetCurrentMillis()
+        {
+            // Looks like fair C# alternative of Java System.currentTimeMillis
+            return DateTime.Now.Ticks/TimeSpan.TicksPerMillisecond;
+        }
+
+        public static void Start(string name)
+        {
+            ExecutionTimes.Add(name, GetCurrentMillis());
+        }
+
+        public static double End(string name)
+        {
+            double time = (GetCurrentMillis() - ExecutionTimes[name]/1000);
+            ExecutionTimes.Remove(name);
+            return time;
+        }
+
+        public static Dictionary<string, long> ExecutionTimes => _executionTimes;
+    }
+}
