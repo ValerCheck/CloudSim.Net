f00487797a3d1f8ac1e25e117a948c3da5cddd1f
vkolesnik <klevalera@gmail.com>
2018-07-07 15:44:31 +0300

    Add ParameterException class.

---
+++ CloudSim.Sharp/ParameterException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CloudSim.Sharp
+{
+    public class ParameterException : Exception
+    {
+        private static readonly long _serialVersionUID = 1;
+
+        private readonly string _message;
+
+        public ParameterException()
+        {
+            _message = null;
+        }
+
+        public ParameterException(string message)
+        {
+            _message = message;
+        }
+
+        public override string ToString()
+        {
+            return _message;
+        }
+    }
+}
